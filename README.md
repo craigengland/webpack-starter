7f8267786bcfbff2177545306cdd9819be852368
Craig England <craig.england@krystal.uk>
2021-04-08 10:53:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Webpack Starter
+
+## Setup
+
+1. `npm install`
+2. `npm run dev` for development environment
+3. `npm run build` for distribution files
+4. `npm run test` for running tests with Jest
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const libraryName = 'Name';
+
+module.exports = {
+  entry: ['./src/main.js'],
+  mode: 'none',
+  plugins: [new MiniCssExtractPlugin({ filename: '[name].css' })],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+        parallel: true,
+      }),
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: '[name].js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 4000,
+    publicPath: 'http://localhost:4000/dist/',
+  },
+};
